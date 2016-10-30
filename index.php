dd468a4e48f25353e04462f4e6eb4043902e6d6e
Valeriy Kolomiets <vel202007@gmail.com>
2016-10-31 01:09:49 +0200

    add Interface

--- index.php
+++ index.php
@@ -4,3 +4,9 @@
     /*
      * Insert new transport from web interface to database
      */
+
+    $data = new \Project\Data\Data;
+    $transport = new \Project\Transport;
+
+    //Add data to database
+    $transport->add($data->get($type, $color, $capacity, $fuel, $price, $comment));
---
+++ src/Transport.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Project;
+
+use Project\Interfaces\TransportInterface;
+
+class Transport implements TransportInterface
+{
+    /**
+     * @param array $data and insert them to DB
+     */
+    public function add(array $data)
+    {
+	//insert array $data into database
+    }
+}
